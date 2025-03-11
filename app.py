cf90bf23676763de70ced6c15510027262f1d7df
Hachiru <92619401+chihakuro@users.noreply.github.com>
2025-03-12 00:43:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 from transformers import pipeline
 
-generator = pipeline('text-generation', model='gpt2')
+generator = pipeline('text-generation', model='meta-llama/Meta-Llama-3-8B')
 
 def generate_text(prompt, max_length):
     generated_text = generator(prompt, max_length=max_length, num_return_sequences=1)
@@ -9,7 +9,7 @@ def generate_text(prompt, max_length):
 
 iface = gr.Interface(
     fn=generate_text,
-    inputs=[gr.Textbox(lines=2, placeholder="Nhập văn bản đầu vào"), gr.Slider(minimum=10, maximum=1000, default=50, label="Độ dài văn bản")],
+    inputs=[gr.Textbox(lines=2, placeholder="Nhập văn bản đầu vào"), gr.Slider(minimum=10, maximum=1000, value=50, label="Max Length")],
     outputs="text",
     title="Ứng dụng Generative AI",
     description="Tạo văn bản với GPT-2",
